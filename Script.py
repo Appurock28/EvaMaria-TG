dd75f653c739d7e4d31c762b72801a317ac9b278
Appurock28 <97966174+Appurock28@users.noreply.github.com>
2022-04-04 18:09:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,17 +1,17 @@
 class script(object):
     START_TXT = """<b>Hello Nanba/Nanbi {}👋
-My Name Is <a href=https://t.me/{}>{}</a>, I Can Provide Movies For ,\n\n👨‍💻 Developer : <a href='https://t.me/Beast_On_Mode'>✯°• Iᴍ 𝐁ᴇᴀsᴛ •°✯ «​ »</a></b>"""
+My Name Is <a href=https://t.me/{}>{}</a>, I Can Provide Movies For ,\n\n👨‍💻 Developer : <a href='https://t.me/Beast_On_Mode'>✯°• Iᴍ 𝐁ᴇᴀsᴛ •°✯​ »</a></b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/Beast_On_Mode'>✯°• Iᴍ 𝐁ᴇᴀsᴛ Via •°✯ «​ »</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/Beast_On_Mode'>✯°• Iᴍ 𝐁ᴇᴀsᴛ Via •°✯​ »</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
+- This Bot Made By Disney Official Linkz. 
 - Source - https://t.me/Disney_Linkz
 
 <b>DEVS:</b>
