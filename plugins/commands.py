d7b960ef0a83682ae048722fc65e6929e2c700b0
Appurock28 <97966174+Appurock28@users.noreply.github.com>
2022-04-07 07:48:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,6 +41,8 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[ 
             InlineKeyboardButton('🔍 Search Movies', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('🎞 Main Channel', url='https://t.me/Disney_Linkz'),
@@ -81,6 +83,8 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],[
             InlineKeyboardButton('🔍 Search Movies', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('🎞 Main Channel', url='https://t.me/Disney_Linkz'),
